f95dec3b27f26021c07e2857012eefe1880b653a
ginnyzou2015 <ginnyzou2015@gmail.com>
2015-10-21 18:21:13 -0400

    dynamic beacon response

--- app/src/main/java/com/demanddev/brandeis/dw/HomeActivity.java
+++ app/src/main/java/com/demanddev/brandeis/dw/HomeActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -13,12 +14,21 @@ import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
 
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
 
 /**
  * Created by jingzou on 10/11/15.
@@ -26,11 +36,12 @@ import java.util.UUID;
 public class HomeActivity extends AppCompatActivity {
     private SharedPreferences mPreferences;
     private static final String TASKS_URL = "https://devweb.herokuapp.com/api/v1/tasks";
+    private final static String LOGIN_API_ENDPOINT_URL = "https://devweb.herokuapp.com/api/v1/sessions.json";
 
-    private String beaconID;
-    private String beaconRecord;
+    private String BeaconID;
     private BeaconManager beaconManager;
     private Region region;
+    private String oldBeaconID = " ";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,20 +49,31 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         mPreferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
 
-        beaconManager = new BeaconManager(this);
+        if (!mPreferences.contains("AuthToken")) {
+            Intent intent = new Intent(HomeActivity.this, WelcomeActivity.class);
+            startActivityForResult(intent, 0);
+        }
+        else {
+            beaconManager = new BeaconManager(this);
 
-        region = new Region("ranged region",
-                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+            region = new Region("ranged region",
+                    UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
 
-        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
-            @Override
-            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
-                if (!list.isEmpty()) {
-                    Beacon nearestBeacon = list.get(0);
-                    beaconID = String.format("B9407F30-F5F8-466E-AFF9-25556B57FE6D:%d:%d", nearestBeacon.getMajor(), nearestBeacon.getMinor());
+            beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+                @Override
+                public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                    if (!list.isEmpty()) {
+                        Beacon nearestBeacon = list.get(0);
+                        BeaconID = String.format("B9407F30-F5F8-466E-AFF9-25556B57FE6D:%d:%d", nearestBeacon.getMajor(), nearestBeacon.getMinor());
+                    }
+                    if (!BeaconID.equals(oldBeaconID)) {
+                        updateTasksFromAPI(LOGIN_API_ENDPOINT_URL, BeaconID);
+                        oldBeaconID = BeaconID;
+                    }
                 }
-            }
-        });
+            });
+            loadTasksFromAPI(TASKS_URL);
+        }
 
     }
 
@@ -61,12 +83,67 @@ public class HomeActivity extends AppCompatActivity {
         getTasksTask.execute(url + "/" + mPreferences.getString("User", "beacontest4@example.com") + "?auth_token=" + mPreferences.getString("AuthToken", ""));
     }
 
-    private void check() {
-        System.out.println("AGAIN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        Intent intent2 = new Intent(HomeActivity.this, MainActivity.class);
-        startActivityForResult(intent2, 0);
-        loadTasksFromAPI(TASKS_URL);
+    private void updateTasksFromAPI(String url, String BeaconID) {
+        LoginTask loginTask = new LoginTask(HomeActivity.this);
+        loginTask.setMessageLoading("Next Beacon...");
+        loginTask.execute(url);
     }
+
+    private class LoginTask extends com.demanddev.brandeis.dw.UrlJsonAsyncTask {
+        public LoginTask(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(urls[0]);
+            JSONObject holder = new JSONObject();
+            JSONObject userObj = new JSONObject();
+            String response = null;
+            JSONObject json = new JSONObject();
+
+            try {
+                try {
+                    // setup the returned values in case
+                    // something goes wrong
+                    json.put("success", false);
+                    json.put("info", "Something went wrong. Retry!");
+                    // add the user email and password to
+                    // the params
+                    System.out.println("@@@@@@@@@" + BeaconID);
+                    userObj.put("beacon_id", BeaconID);
+                    userObj.put("email", mPreferences.getString("User", "beacontest4@example.com"));
+                    userObj.put("password", mPreferences.getString("Password", "secret12345"));
+                    holder.put("user", userObj);
+                    StringEntity se = new StringEntity(holder.toString());
+                    post.setEntity(se);
+
+                    // setup the request headers
+                    post.setHeader("Accept", "application/json");
+                    post.setHeader("Content-Type", "application/json");
+
+                    ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                    response = client.execute(post, responseHandler);
+                    json = new JSONObject(response);
+
+                } catch (HttpResponseException e) {
+                    e.printStackTrace();
+                    Log.e("ClientProtocol", "" + e);
+                    json.put("info", "Email and/or password are invalid. Retry!");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("IO", "" + e);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("JSON", "" + e);
+            }
+
+            return json;
+        }
+    }
+
 
 
     private class GetTasksTask extends com.demanddev.brandeis.dw.UrlJsonAsyncTask {
@@ -108,24 +185,15 @@ public class HomeActivity extends AppCompatActivity {
                 beaconManager.startRanging(region);
             }
         });
+        /*
         if (mPreferences.contains("AuthToken")) {
             loadTasksFromAPI(TASKS_URL);
-            //beaconRecord = mPreferences.getString("Beacon", "");
-            //System.out.println("&&&&&&&" + beaconRecord);
-            //System.out.println("@@@@@" + beaconID);
-            //if (!beaconRecord.equals("B9407F30-F5F8-466E-AFF9-25556B57FE6D:10053:63975")) {
-                //check();
-            //}
         } else {
             Intent intent = new Intent(HomeActivity.this, WelcomeActivity.class);
             startActivityForResult(intent, 0);
         }
+        */
     }
 
 
-    @Override
-    protected void onPause() {
-        beaconManager.stopRanging(region);
-        super.onPause();
-    }
 }
